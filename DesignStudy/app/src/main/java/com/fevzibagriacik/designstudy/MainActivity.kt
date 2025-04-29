5f4c8074d703d336401fc4cd952655b02ddf9f13
Fevzi <fevzi.bagriacik1905@gmail.com>
2025-04-29 22:11:36 +0300

    Lesson-4 is added

--- DesignStudy/app/src/main/java/com/fevzibagriacik/designstudy/MainActivity.kt
+++ DesignStudy/app/src/main/java/com/fevzibagriacik/designstudy/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //R = res (resources) folder
+        //R = (in kotlin classes) - res folder, resources
+        //@ = (in xml file) - res folder, resources
     }
 }
